82e2c4dbbb1f6620758ffab17412aa710d48e02e
radhikabanerjea <radhika23rt@gmail.com>
2022-02-07 08:47:25 -0500

    second commit

--- Lists.java
+++ Lists.java
@@ -1,3 +1,8 @@
+/*Radhika Banerjea
+ * February 7, 2021
+ * pop up window for all the encoding options
+ */
+//importing what's necessary
 import java.io.IOException;
 
 import org.eclipse.swt.widgets.Display;
@@ -9,10 +14,16 @@ import org.eclipse.swt.widgets.List;
 public class Lists {
 	protected Shell shlAllPossibleGuesses;
 
-	
+	/**
+	 * 
+	 * @param lists the list of all the possible decoding options
+	 * @throws IOException
+	 */
 	public Lists (String lists) throws IOException {
 		try {
+			//creating the display.
 			Display display = Display.getDefault();
+			//going to the area that makes the shell.
 			createContents(lists);
 			shlAllPossibleGuesses.open();
 			shlAllPossibleGuesses.layout();
@@ -27,21 +38,18 @@ public class Lists {
 	}
 
 
-	/**
-	 * Open the window.
-	 */
-	public void open() {
-		
-	}
+
 
 	/**
 	 * Create contents of the window.
 	 */
 	protected void createContents(String lists) {
+		//creating the shell, and the dimensions of the shell for lists
 		shlAllPossibleGuesses = new Shell();
 		shlAllPossibleGuesses.setSize(320, 602);
 		shlAllPossibleGuesses.setText("All Possible Guesses");
 		
+		//dimeensions of the label that contains all the info of the guesses.
 		Label lblList = new Label(shlAllPossibleGuesses, SWT.NONE);
 		lblList.setBounds(10, 10, 188, 523);
 		lblList.setText(lists);
